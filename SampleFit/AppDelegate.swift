7ee5400a14e83aeb5a6228d12bbcc00f686b9f5e
Juan Pedro Lozano <jpyreaction@gmail.com>
2018-07-10 19:06:36 +0200

    Clean up

---
+++ SampleFit/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SampleFit
+//
+//  Created by Juan Pedro Lozano Baño on 10/7/18.
+//  Copyright © 2018 Juan Pedro Lozano Baño. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		return true
+	}
+}
+
